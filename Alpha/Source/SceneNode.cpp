6ec47c032b61ca0b097c9a7aa6c434e03e1cb584
blazetrinity <blaze-trinity@hotmail.sg>
2015-11-19 19:06:05 +0800

    SceneNode 3/4 Completed, Testing not done, GetChildNode function needs checking

--- Alpha/Source/SceneNode.cpp
+++ Alpha/Source/SceneNode.cpp
@@ -1,11 +1,54 @@
 #include "SceneNode.h"
 
 
-SceneNode::SceneNode()
+SceneNode::SceneNode() : gameObject3D(NULL), parentNode(NULL)
 {
 }
 
 
 SceneNode::~SceneNode()
 {
+}
+
+void SceneNode::SetGameObject(GameObject3D *gameObject)
+{
+	this->gameObject3D = gameObject;
+}
+
+void SceneNode::SetParentNode(SceneNode* _parentNode)
+{
+	this->parentNode = _parentNode;
+}
+
+void SceneNode::AddChildNode(SceneNode *childNode)
+{
+	childNode->SetParentNode(this);
+
+	this->childeNodes.push_back(childNode);
+}
+
+void SceneNode::AddChildToChildNode(string childName, SceneNode *childNode)
+{
+	this->GetChildNode(childName)->AddChildNode(childNode);
+}
+
+SceneNode* SceneNode::GetChildNode(string childName)
+{
+	for (int i = 0; i < childeNodes.size(); ++i)
+	{
+		if (childeNodes[i]->GetGameObject()->getName() == childName)
+		{
+			return childeNodes[i];
+		}
+
+		else
+		{
+			return childeNodes[i]->GetChildNode(childName);
+		}
+	}
+}
+
+GameObject3D* SceneNode::GetGameObject()
+{
+	return this->gameObject3D;
 }
--- Alpha/Source/SceneNode.h
+++ Alpha/Source/SceneNode.h
@@ -1,11 +1,28 @@
 #ifndef SCENE_NODE_H
 #define SCENE_NODE_H
 
+#include "GameObject3D.h"
+
 class SceneNode
 {
 public:
 	SceneNode();
 	~SceneNode();
+	
+	void SetGameObject(GameObject3D *gameObject);
+	void SetParentNode(SceneNode* _parentNode);
+
+	void AddChildNode(SceneNode *childNode);
+	void AddChildToChildNode(string childName, SceneNode *childNode);
+
+	SceneNode* GetChildNode(string childName);
+	
+	GameObject3D* GetGameObject();
+
+protected:
+	GameObject3D* gameObject3D;
+	SceneNode *parentNode;
+	vector<SceneNode*> childeNodes;
 };
 
 #endif
