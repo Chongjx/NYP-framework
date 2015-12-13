347fb1ba2d03f818fb71d36642bcfb9606215574
Princeton Chew <PrincetonCYK@hotmail.com>
2015-12-13 19:17:16 +0900

    More legit weapon class

--- Alpha/Source/Weapon.cpp
+++ Alpha/Source/Weapon.cpp
@@ -4,7 +4,11 @@ Weapon::Weapon() :
 	RELOAD_TIME(0.f),
 	FIRE_RATE(0.f),
 	reload_timer(0.f),
-	fire_timer(0.f)
+	fire_timer(0.f),
+	Current_Ammo(0),
+	Reserve_Ammo(0),
+	Reloading(false),
+	MAX_CURRENT_AMMO(0)
 {
 
 }
@@ -14,7 +18,9 @@ Weapon::~Weapon()
 
 }
 
-void Weapon::Init(GameObject3D weapon, CProjectile type, const float RELOAD_TIME, const float FIRE_RATE)
+void Weapon::Init(GameObject3D weapon, CProjectile type, 
+	const int startingAmmo, const int reserveAmmo,
+	const float RELOAD_TIME, const float FIRE_RATE)
 {
 	this->setPosition(weapon.getPosition());
 	this->setCollidable(weapon.getCollidable());
@@ -25,11 +31,77 @@ void Weapon::Init(GameObject3D weapon, CProjectile type, const float RELOAD_TIME
 
 	this->projectile_type = type;
 
+	this->MAX_CURRENT_AMMO = startingAmmo;
+	this->Current_Ammo = startingAmmo;
+	this->Reserve_Ammo = reserveAmmo;
+
 	this->RELOAD_TIME = RELOAD_TIME;
 	this->FIRE_RATE = FIRE_RATE;
+}
+
+void Weapon::Update(double dt)
+{
+	this->fire_timer += (float)dt;
+
+	//Only if reloading weapon
+	if (Reloading){
+		this->reload_timer += (float)dt;
+		if (reload_timer >= RELOAD_TIME){
+			reload_timer = 0.f;
+			RefillWeapon();
+			Reloading = false;
+		}
+	}
 }
 
 CProjectile Weapon::getProjectile(void)
 {
 	return this->projectile_type;
+}
+
+int Weapon::getCurrentAmmo(void)
+{
+	return this->Current_Ammo;
+}
+
+int Weapon::getReservedAmmo(void)
+{
+	return this->Reserve_Ammo;
+}
+void Weapon::setReservedAmmo(int value)
+{
+	this->Reserve_Ammo = value;
+}
+
+void Weapon::RefillWeapon()
+{
+	unsigned difference = MAX_CURRENT_AMMO - Current_Ammo;
+
+	if (difference <= Reserve_Ammo)
+	{
+		Reserve_Ammo -= difference;
+		Current_Ammo += difference;
+	}
+	if (difference > Reserve_Ammo)
+	{
+		Current_Ammo += Reserve_Ammo;
+		Reserve_Ammo = 0;
+	}
+}
+
+bool Weapon::Fire()
+{
+	if (this->fire_timer >= FIRE_RATE && !Reloading)
+	{
+		this->fire_timer = 0.f;
+		return true;
+	}
+	return false;
+}
+
+void Weapon::Reload()
+{
+	if (Current_Ammo < MAX_CURRENT_AMMO && Reserve_Ammo > 0){
+		this->Reloading = true;
+	}
 }
--- Alpha/Source/Weapon.h
+++ Alpha/Source/Weapon.h
@@ -10,16 +10,36 @@ public:
 	Weapon();
 	~Weapon();
 
-	void Init(GameObject3D weapon, CProjectile type, const float RELOAD_TIME = 2.f, const float FIRE_RATE = 0.1f);	//Init all variables
+	void Init(GameObject3D weapon, CProjectile type, 
+		const int startingAmmo = 30, const int reserveAmmo = 90,
+		const float RELOAD_TIME = 2.f, const float FIRE_RATE = 0.1f);	//Init all variables
 
 	CProjectile getProjectile(void);
+	
+	void Update(double dt);	//Update weapon
 
+	int getCurrentAmmo(void);	//Get current amount of ammo
+
+	int getReservedAmmo(void);	//Get reserved amount of ammo
+	void setReservedAmmo(int value);	//Set reserved amount of ammo
+
+	bool Fire();	//Fire Weapon
+	void Reload();	//Reload Weapon
+	
 private:
+	void RefillWeapon();	//Refill Weapon ammo
+
 	float RELOAD_TIME;	//Reload time
 	float FIRE_RATE;	//Rate of fire
 
 	float reload_timer;	//used to keep track
 	float fire_timer;	//used to keep track
+
+	int MAX_CURRENT_AMMO;	//Maximum amount of ammo
+	int Current_Ammo;	//Current amount of ammo
+	int Reserve_Ammo;	//Reserved amount of ammo
+
+	bool Reloading;	//Reloading status of weapon
 
 	CProjectile projectile_type;
 
